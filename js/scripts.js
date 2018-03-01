fcdc31629b0808d767a95b21efb912b62029a6c3
Ian Andreu <Ian@Shivanis-MacBook-Pro.local>
2018-03-01 10:03:36 -0500

    bowie smells

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+console.log('we good');
+var BDC ={};
+
+BDC.linkHover = {
+	
+}
+
+
+
+$(window).scroll(function() {
+
+});
+
+$(window).load(function(){
+
+});
+$(document).ready(function() {
+
+});
