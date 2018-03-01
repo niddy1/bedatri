8c2823ba23204ca20dfc0f3f8799cd31c4fbf926
Ian Andreu <Ian@Shivanis-MacBook-Pro.local>
2018-03-01 13:40:32 -0500

    site mainly complete, still need to update real content and fix some links

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 console.log('we good');
 var BDC ={};
 
-BDC.linkHover = {
+BDC.aboutLinkAction = {
 	
-}
 
+	init: function(){
+		$aboutLink = jQuery('.about-link');
+		$aboutContainer = jQuery('.about-container');
+		$articleContainer = jQuery('.article-container');
+
+		var self = this;
+		
+		$aboutLink.click(function(e){
+			e.preventDefault();
+			$aboutContainer.toggle();
+			$articleContainer.toggle();
+		})
+	}
+}
 
 
 $(window).scroll(function() {
@@ -12,8 +25,14 @@ $(window).scroll(function() {
 });
 
 $(window).load(function(){
-
+	
 });
 $(document).ready(function() {
+	BDC.aboutLinkAction.init();
+	// jQuery('.about-link').click(function(e){
+	// 			e.preventDefault();
+	// 			alert('hiii');
+
+	// 		})
 
 });
